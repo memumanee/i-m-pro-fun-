a1de8b7541b156ff6414e8a62ceefc920eaa3cb3
memumanee <prapassorn@prapassorns-MacBook-Air.local>
2021-08-11 00:17:09 +0700

    week1work1

---
+++ week1_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("num1:");
+    scanf("%d",&a);
+    printf("num2:");
+    scanf("%d",&b);
+    printf("+_%d",a+b);
+    printf("-_%d",a-b);
+    printf("x_%d",a*b);
+    printf("+_%d",a/b);
+}
